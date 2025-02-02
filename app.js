4a9939f5654caebcb1f4c61aae0c3cda0f9facfe
Vikas94a <vikas1.kodehode@gmail.com>
2025-02-02 18:55:40 +0100

    update JavaScript

---
+++ app.js
@@ -0,0 +1,19 @@
+const toggleMenu = document.querySelector("#toggle-menu")
+const burgerMenu = document.querySelector("#nav-menu")
+const cursor = document.querySelector("#circle-cursor") 
+
+
+
+
+toggleMenu.addEventListener("click", ()=>{
+if(burgerMenu.style.display === "" || burgerMenu.style.display === 'none' ){
+  burgerMenu.style.display = "block"
+}else if(burgerMenu.style.display === "block"){
+ burgerMenu.style.display = "none"
+}
+})
+
+document.addEventListener('mousemove', (e)=>{
+   cursor.style.left=e.pageX - (cursor.offsetWidth /2) +"px";
+   cursor.style.top =e.pageY - (cursor.offsetHeight /2) +"px"
+})
